25a9f1df26240ce6931a5763ef06b57d82c2d3cb
rangarajan <rangarajan@laptop.com>
2021-06-22 12:28:43 +0530

    dist files

---
+++ dist/index.modern.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+var styles = {"test":"_styles-module__test__3ybTi"};
+
+const ExampleComponent = ({
+  text,
+  styleObj
+}) => {
+  return /*#__PURE__*/React.createElement("div", {
+    className: styles.test,
+    style: styleObj
+  }, "Example Component: ", text);
+};
+
+export { ExampleComponent };
+//# sourceMappingURL=index.modern.js.map
